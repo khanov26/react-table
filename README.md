95d1806be98b6c453ba297c3c85d3d3004c81471
linar <khanov26@gmail.com>
2021-09-21 11:30:25 +0300

    firebase deploying

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Тестовое задание: https://github.com/fugr-ru/frontend-javascript-test
+
+Демо: https://react-table-df2bd.web.app/
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -35,10 +35,10 @@ const Table: React.FC<Props> = props => {
             let url;
             switch (dataSize) {
                 case "small":
-                    url = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
+                    url = "https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
                     break;
                 case "large":
-                    url = "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
+                    url = "https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
                     break;
             }
 
